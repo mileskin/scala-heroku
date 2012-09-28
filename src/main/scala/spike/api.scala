196b65bbe1c7e6c9f036763937a702685ed4e004
Miki Leskinen <miki.leskinen@reaktor.fi>
2012-09-28 21:10:09 +0300

    Pass page title to html template
    Signed-off-by: Miki Leskinen <miki.leskinen@reaktor.fi>

--- src/main/scala/spike/api.scala
+++ src/main/scala/spike/api.scala
@@ -5,12 +5,12 @@ import scalate.ScalateSupport
 
 class MyScalatraFilter extends ScalatraFilter with ScalateSupport {
   get("/") {
-    html("foo")
+    html("foo", "Index Page")
   }
 
-  private def html(template: String) = {
+  private def html(template: String, title: String) = {
     contentType = "text/html"
-    ssp("/WEB-INF/scalate/views/%s.ssp".format(template), "layout" -> "/WEB-INF/scalate/layouts/default.ssp")
+    ssp("/WEB-INF/scalate/views/%s.ssp".format(template), "layout" -> "/WEB-INF/scalate/layouts/default.ssp", "title" -> title)
   }
 }
 
